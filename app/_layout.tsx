d44e2e93eca4ad0a48fb2b19d7888390ea0a55a4
mirsuhail029@gmail.com <mirsuhail029@gmail.com>
2026-01-11 14:27:51 +0530

    setup login screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  const isAuthenticated = false;
+  return (
+    <Stack>
+      <Stack.Protected guard={isAuthenticated}>
+        <Stack.Screen
+          name="(tabs)"
+          options={{ title: "Home", headerShown: false }}
+        />
+      </Stack.Protected>
+      <Stack.Protected guard={!isAuthenticated}>
+        <Stack.Screen name="login" />
+      </Stack.Protected>
+    </Stack>
+  );
 }
---
+++ app/login.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import {
+  Alert,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+const login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handlePress = () => {
+    if (username.trim() === "" || password.trim() === "") {
+      Alert.alert("Error", "Both fields required");
+    }
+    console.log(username, password, "pressed");
+  };
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Username"
+        style={styles.userInputFields}
+        onChangeText={setUsername}
+        value={username}
+      />
+      <TextInput
+        placeholder="Password"
+        style={styles.userInputFields}
+        onChangeText={setPassword}
+        value={password}
+      />
+      <Pressable style={styles.button} onPress={handlePress}>
+        <Text style={styles.buttonLabel}>Sign in</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default login;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    gap: 10,
+  },
+  userInputFields: {
+    borderWidth: 1,
+    borderColor: "lightgray",
+    borderRadius: 8,
+  },
+  button: {
+    backgroundColor: "lightblue",
+    borderWidth: 1,
+    alignSelf: "baseline",
+    padding: 10,
+    borderRadius: 8,
+    borderColor: "lightblue",
+    boxShadow: "0px 3px 20px 1px rgba(0,0,0,0.1)",
+  },
+  buttonLabel: {
+    color: "#ffffff",
+  },
+});
